2493c75feb99feac04ed267704847adb34c225b7
felipe bernal <141671878+juanfelipeber@users.noreply.github.com>
2025-05-30 16:49:01 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,13 @@
 function calculateNumbers(var1 ,var2) {
     return var1 + var2;
 }
+
+function powerOfNumber(base, exponent) {
+    return Math.pow(base, exponent);
+}
+
+function calculateSquareRoot(number) {
+    return Math.sqrt(number);
+}
+
+
